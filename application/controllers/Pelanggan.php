1be8a2b44dbe6b49a886d4ae7c346eb2cf9fb39d
Yuki <tantoyk7@gmail.com>
2024-03-19 01:26:17 +0700

    FINISH ALL

--- application/controllers/Pelanggan.php
+++ application/controllers/Pelanggan.php
@@ -1,5 +1,7 @@
 <?php
 defined('BASEPATH') or exit ('No direct script access allowed');
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
 class Pelanggan extends CI_Controller
 {
@@ -25,6 +27,58 @@ class Pelanggan extends CI_Controller
 		$this->load->view('pages/pelanggan/index.php', $data);
 		$this->load->view('layout/footer.php');
 	}
+	public function export_excel(){
+		$data = $this->Bukukas_model->get_pelanggan();
+		$spreadsheet = new Spreadsheet();
+        $sheet = $spreadsheet->getActiveSheet();
+
+        // Tulis header "LAPORAN DATA SISWA" dan "KOBER PAUD DARUSHOWAAB"
+        $sheet->setCellValue('A1', 'Data Kontak Pelanggan');
+        $sheet->mergeCells('A1:D1');
+        $sheet->getStyle('A1')->getAlignment()->setHorizontal('center');
+        $sheet->getStyle('A1')->getFont()->setBold(true)->setSize(13);
+
+        // Tulis header kolom
+        $header = array('No', 'Nama Pelanggan', 'Alamat', 'No HP');
+        $column = 'A';
+        $row = 2;
+
+        foreach ($header as $item) {
+            $sheet->setCellValue($column . $row, $item);
+            $sheet->getColumnDimension($column)->setAutoSize(false); // Tambahkan baris ini
+            $sheet->getColumnDimension($column)->setWidth(15); // Atur lebar kolom (misalnya 30)
+            if ($item === 'No') {
+                $sheet->getColumnDimension($column)->setAutoSize(false); // Tambahkan baris ini
+                $sheet->getColumnDimension($column)->setWidth(4); // Atur lebar kolom (misalnya 30)
+            }
+			if ($item === 'Alamat') {
+                $sheet->getColumnDimension($column)->setAutoSize(false); // Tambahkan baris ini
+                $sheet->getColumnDimension($column)->setWidth(30); // Atur lebar kolom (misalnya 30)
+            }
+            $column++;
+        }
+        $row++;
+        $no = 1;
+
+        foreach ($data as $item) {
+            $sheet->setCellValue('A' . $row, $no);
+            $sheet->setCellValue('B' . $row, $item['nama_pelanggan']);
+            $sheet->setCellValue('C' . $row, $item['alamat']);
+            $sheet->setCellValue('D' . $row, $item['no_telp']);
+            $no++;
+            $row++;
+        }
+        $writer = new Xlsx($spreadsheet);
+        $filename = 'Data Kontak Pelanggan.xlsx';
+
+        header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+        header('Content-Disposition: attachment;filename="' . $filename . '"');
+        header('Cache-Control: max-age=0');
+
+        $writer->save('php://output');
+        exit();
+
+	} 
 
 }
 
